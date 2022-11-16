50699f014738b9e117ae9072ddb3e5325b010871
Mike Oram <Mikeoram01@gmail.com>
2022-11-16 16:37:34 +0000

    Completed the card expiry forms and input in JS

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const cvcInput = document.getElementById('card-form-card-cvc');
 const expiryMonthOnCardFront = document.getElementById('card-front-expiry-month');
 const expiryYearOnCardFront = document.getElementById('card-front-expiry-year');
 const monthExpiryInput = document.getElementById('card-form-card-expiry-month');
-const yearExpiryInput = document.getElementById('card-form-year-expiry');
+const yearExpiryInput = document.getElementById('card-form-card-expiry-year');
 const cvcError = document.getElementById('cvc-error');
 
 function sanitizeInputLength(input, output, length) {
@@ -13,18 +13,24 @@ function sanitizeInputLength(input, output, length) {
 }
 
 
-function inputHandlerExpiryMonth(event) {
+const inputHandlerExpiryMonth = (event) => {
     sanitizeInputLength(event.target.value, monthExpiryInput, 2);
     expiryMonthOnCardFront.innerHTML = event.target.value;
 }
 
-function inputHandlerCVC(event) {
+const inputHandlerExpiryYear = (event) => {
+    sanitizeInputLength(event.target.value, yearExpiryInput, 2);
+    expiryYearOnCardFront.innerHTML = event.target.value;
+}
+
+const inputHandlerCVC = (event) => {
     sanitizeInputLength(event.target.value, cvcInput, 3);
     cvcOnCardBack.innerHTML = event.target.value;
 }
 
 cvcInput.addEventListener('input', inputHandlerCVC);
-monthExpiryInput.addEventListener('input', inputHandlerExpiryMonth)
+monthExpiryInput.addEventListener('input', inputHandlerExpiryMonth);
+yearExpiryInput.addEventListener('input', inputHandlerExpiryYear);
 
 
 //TODO Sanitize when input submitted !
